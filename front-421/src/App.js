0c91220c393e1a0183fcb33026cd33a2483bdbf9
TIJI <haugueltanguypub@gmail.com>
2023-02-21 14:36:13 +0100

    kush crew logo

--- front-421/src/App.js
+++ front-421/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import kush_crew_logo from "./assets/images/logo_kc.png";
 
 function App() {
   return (
@@ -7,6 +8,9 @@ function App() {
         <a>
           ça part de là
         </a>
+
+      <h1>421</h1>
+      <img src={kush_crew_logo} alt="logo KUSH CREW" />
       </header>
     </div>
   );
