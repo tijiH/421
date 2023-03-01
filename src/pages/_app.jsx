34a215cb44d08ccf27d18a7193929c14c6483a60
= <=>
2023-03-01 20:59:30 +0100

    changement icone

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }) {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>421</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
